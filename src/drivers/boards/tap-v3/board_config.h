33750b181ec37f4f27757a288c05cac51fa2dcb2
David Sidrane <david_s5@nscdg.com>
2017-10-06 07:16:56 -1000

    tap-v3:board_config Add EEPROM WP control and MTD_PARTITION_OVERRIDE
       This is a deviation from FMUv5 Spec PB3 is used as WP pin
       on AT24XX EEPROM for the cal data.

--- src/drivers/boards/tap-v3/board_config.h
+++ src/drivers/boards/tap-v3/board_config.h
@@ -3,6 +3,10 @@
 #undef BOARD_HAS_LTC4417
 #define PX4_FMUV5_RC00
 
+#define GPIO_EEPROM_WP  (GPIO_OUTPUT|GPIO_PUSHPULL|GPIO_SPEED_50MHz|GPIO_OUTPUT_SET|GPIO_PORTB|GPIO_PIN2)
+#define BOARD_EEPROM_WP_CTRL(_on_true)    px4_arch_gpiowrite(GPIO_EEPROM_WP, (_on_true))
+#define BOARD_HAS_MTD_PARTITION_OVERRIDE {"/fs/mtd_caldata"}
+
 #include <drivers/boards/px4fmu-v5/board_config.h>
 
 #undef BOARD_NAME

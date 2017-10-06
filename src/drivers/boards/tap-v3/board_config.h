388054c43a2cb247356850eb619dee09f46033f0
David Sidrane <david_s5@nscdg.com>
2017-10-05 16:34:55 -1000

    tap-v3:Reorder define and inclusion
        We define PX4_FMUV5_RC00 and undefine BOARD_HAS_LTC4417
        then include the common header.

--- src/drivers/boards/tap-v3/board_config.h
+++ src/drivers/boards/tap-v3/board_config.h
@@ -1,9 +1,9 @@
 #pragma once
 
-#include <drivers/boards/px4fmu-v5/board_config.h>
-
 #undef BOARD_HAS_LTC4417
-#define PX4_FMUV5_RC01
+#define PX4_FMUV5_RC00
+
+#include <drivers/boards/px4fmu-v5/board_config.h>
 
 #undef BOARD_NAME
 #define	BOARD_NAME "TAP_V3"

df0033a61f27d77f03414f16a6206a7f0ac2bde8
Matthias Grob <maetugr@gmail.com>
2017-11-15 08:56:11 +0100

    Boardconfig: enable usage of ESC current readings on tap-v3 hardware

--- src/drivers/boards/tap-v3/board_config.h
+++ src/drivers/boards/tap-v3/board_config.h
@@ -48,6 +48,7 @@
 
 #define TAP_ESC_NO_VERIFY_CONFIG
 #define BOARD_TAP_ESC_MODE 2 // select closed-loop control mode for the esc
+#define BOARD_USE_ESC_CURRENT_REPORT // each ESC reports its current estimate
 
 #define PX4_I2C_BUS_ONBOARD 4 // wrong workaround to get the baro working
 

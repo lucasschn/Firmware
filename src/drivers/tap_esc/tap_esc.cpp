lucasschn/Firmware

commit 3d869034e1baeebdae311623679f60913f23a933

snapdragon-tap-esc: working version, needs cleanup and correct cmake

src/drivers/tap_esc/tap_esc.cpp
@@ -69,6 +69,10 @@
 
 #include "drv_tap_esc.h"
 
+#ifdef __PX4_QURT
+#define BOARD_TAP_ESC_NO_VERIFY_CONFIG
+#endif
+
 #if !defined(BOARD_TAP_ESC_MODE)
 #  define BOARD_TAP_ESC_MODE 0
 #endif
@@ -581,6 +585,11 @@ TAP_ESC::cycle()
 		subscribe();
 		_groups_subscribed = _groups_required;
 		_current_update_rate = 0;
+	}
+
+	if (_groups_required == 0) {
+		// No work to do, for instance when mixer has not been loaded yet.
+		return;
 	}
 
 	unsigned max_rate = _pwm_default_rate ;

cmake/configs/qurt_sdflight_default.cmake
@@ -44,6 +44,8 @@ set(config_module_list
 	# System commands
 	#
 	systemcmds/param
+	systemcmds/mixer
+	systemcmds/motor_test
 
 	#
 	# Estimation modules
@@ -77,6 +79,7 @@ set(config_module_list
 	drivers/spektrum_rc
 	drivers/qshell/qurt
 	drivers/snapdragon_pwm_out
+	drivers/tap_esc
 
 	#
 	# Libraries
@@ -95,6 +98,8 @@ set(config_module_list
 	lib/version
 	lib/DriverFramework/framework
 	lib/FlightTasks
+	lib/led
+	lib/tunes
 
 	#
 	# QuRT port

posix-configs/eagle/flight/px4.config
@@ -3,7 +3,7 @@ qshell start
 param set SYS_AUTOSTART 4001
 param set SYS_MC_EST_GROUP 2
 sleep 1
-gps start -d /dev/tty-4
+#gps start -d /dev/tty-4
 param set MAV_TYPE 2
 df_hmc5883_wrapper start
 df_mpu9250_wrapper start
@@ -16,5 +16,9 @@ ekf2 start
 land_detector start multicopter
 mc_pos_control start
 mc_att_control start
-snapdragon_pwm_out start
+#snapdragon_pwm_out start
+tap_esc start -d /dev/tty-4 -n 4
 spektrum_rc start
+
+sleep 1
+mixer load /dev/tap_esc /dev/fs/quad_x.main.mix

commit cfac2d249a018915f5d3c6658c872c9923befab4

tap_esc: hotfix for tap_esc driver being stuck. Problem signs: No LED, no startup tune, nothing. Bug introduced with 3d869034e1baeebdae311623679f60913f23a933

src/drivers/tap_esc/tap_esc.cpp
@@ -588,11 +588,15 @@ TAP_ESC::cycle()
 		_current_update_rate = 0;
 	}
 
+#ifdef __PX4_QURT
+
 	if (_groups_required == 0) {
 		// This driver has no work to do, for instance when mixer has not been
 		// loaded yet. There is no reason to run through the cycle now.
 		return;
 	}
+
+#endif
 
 	unsigned max_rate = _pwm_default_rate ;
 
dc640e26eb240f36d86de23648f22159f98ed91d
Hao Zhang <hao.zhang@yuneec.com>
2018-02-11 19:00:05 +0800

    ESC binary: Add esc binary for v18s.

--- cmake/configs/nuttx_tap-v4_v18s.cmake
+++ cmake/configs/nuttx_tap-v4_v18s.cmake
@@ -1,5 +1,6 @@
 include(configs/nuttx_tap-v4_default)
 
+px4_nuttx_configure(HWCLASS m7 CONFIG nsh ROMFS y ROMFSROOT tap_common IO V18S)
 # created to enable specific module to be included or excluded from the build
 
 list(REMOVE_ITEM config_module_list

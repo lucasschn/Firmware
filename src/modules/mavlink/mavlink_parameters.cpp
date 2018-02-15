0b674bb1736bc8e3e68aa15bc7e593d8a858053a
Julian Oes <julian@oes.ch>
2018-02-15 13:01:33 +0100

    mavlink_parameters: allow setting the LED mode

--- src/modules/mavlink/mavlink_parameters.cpp
+++ src/modules/mavlink/mavlink_parameters.cpp
@@ -147,7 +147,8 @@ MavlinkParametersManager::handle_message(const mavlink_message_t *msg)
 				    strcmp(name, "GF_ACTION") != 0 &&
 				    strcmp(name, "GF_MAX_HOR_DIST") != 0 &&
 				    strcmp(name, "GF_MAX_VER_DIST") != 0 &&
-				    strcmp(name, "MPC_VEL_MANUAL") != 0) {
+				    strcmp(name, "MPC_VEL_MANUAL") != 0 &&
+				    strcmp(name, "COM_LED_MODE") != 0) {
 					return;
 				}
 

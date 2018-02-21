0b507278a1a960e129c6069db0a4596c602cfd5a
Julian Oes <julian@oes.ch>
2018-02-21 11:20:30 +0100

    navigator: don't continue triggering automatically
    Restarting the triggering is now handled by DataPilot, and we don't need
    to do it on mission activation. Otherwise, this means that we start
    capturing pictures right after takeoff in a second mission because we
    are "continuing" the first mission.

--- src/modules/navigator/mission.cpp
+++ src/modules/navigator/mission.cpp
@@ -148,14 +148,6 @@ void
 Mission::on_activation()
 {
 	set_mission_items();
-
-	// unpause triggering if it was paused
-	vehicle_command_s cmd = {};
-	cmd.command = vehicle_command_s::VEHICLE_CMD_DO_TRIGGER_CONTROL;
-	// unpause trigger
-	cmd.param1 = -1.0f;
-	cmd.param3 = 0.0f;
-	_navigator->publish_vehicle_cmd(&cmd);
 }
 
 void

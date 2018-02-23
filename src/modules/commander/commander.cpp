fcd3318c217028d710a508fe13850a2d0b867b28
Alessandro Simovic <alessandro@yuneecresearch.com>
2018-02-23 08:31:34 +0100

    commander: Decreasing stack size back to 3160 (upstream increased it)
    Upstream increased the commander stack size twice. Let's see if we
    can live without it since memory is already very restricted on the H520.
    
    Upstream:
    https://github.com/PX4/Firmware/commit/976b890c56f2028d33a86ad5f5ea19366eb49c9d
    https://github.com/PX4/Firmware/commit/f69a6af9899a280ff6ce9960fee901defbb9bbc9

--- src/modules/commander/commander.cpp
+++ src/modules/commander/commander.cpp
@@ -4876,7 +4876,7 @@ int Commander::task_spawn(int argc, char *argv[])
 	_task_id = px4_task_spawn_cmd("commander",
 				      SCHED_DEFAULT,
 				      SCHED_PRIORITY_DEFAULT + 40,
-				      3250,
+				      3000,
 				      (px4_main_t)&run_trampoline,
 				      (char *const *)argv);
 

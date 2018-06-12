4ca6ecea8a3ff2d78f84f1d8177d36e951731a3a
acfloria <florian.achermann@mavt.ethz.ch>
2018-06-12 10:27:55 +0200

    Fix: Do not set the closest mission item in normal mission mode

--- src/modules/navigator/mission.cpp
+++ src/modules/navigator/mission.cpp
@@ -149,7 +149,11 @@ void
 Mission::on_activation()
 {
 	if (_mission_waypoints_changed) {
-		_current_offboard_mission_index = index_closest_mission_item();
+		// do not set the closest mission item in the normal mission mode
+		if (_mission_execution_mode != mission_result_s::MISSION_EXECUTION_MODE_NORMAL) {
+			_current_offboard_mission_index = index_closest_mission_item();
+		}
+
 		_mission_waypoints_changed = false;
 	}
 
@@ -185,7 +189,11 @@ Mission::on_active()
 	/* reset mission items if needed */
 	if (offboard_updated || _mission_waypoints_changed || _execution_mode_changed) {
 		if (_mission_waypoints_changed) {
-			_current_offboard_mission_index = index_closest_mission_item();
+			// do not set the closest mission item in the normal mission mode
+			if (_mission_execution_mode != mission_result_s::MISSION_EXECUTION_MODE_NORMAL) {
+				_current_offboard_mission_index = index_closest_mission_item();
+			}
+
 			_mission_waypoints_changed = false;
 		}
 

1b8db86070c0c24c7da7ec44b7af6db46f46386f
Dennis Mannhart <dennis@yuneecresearch.com>
2018-09-11 08:53:14 +0200

    hc_sr04: avoidance is considered on if switch is in middle or on position

--- src/drivers/distance_sensor/hc_sr04/hc_sr04.cpp
+++ src/drivers/distance_sensor/hc_sr04/hc_sr04.cpp
@@ -715,7 +715,8 @@ HC_SR04::cycle()
 			struct manual_control_setpoint_s manual;
 			orb_copy(ORB_ID(manual_control_setpoint), _manual_sub, &manual);
 
-			if (manual.obsavoid_switch == manual_control_setpoint_s::SWITCH_POS_ON) {
+			if (manual.obsavoid_switch == manual_control_setpoint_s::SWITCH_POS_ON ||
+			    manual.obsavoid_switch == manual_control_setpoint_s::SWITCH_POS_MIDDLE) {
 
 				/* The set point message is published periodically; We only want the deltas
 				 * to cause changes in the physical PWM state.

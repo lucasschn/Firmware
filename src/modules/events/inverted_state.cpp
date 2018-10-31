39fc5a35e3a40b20fd7d74869c3082397bdfbb01
Simone Guscetti <simone@yuneecresearch.com>
2018-10-31 14:05:17 +0100

    events: Add inverted state class
    This class handles the detection of the inverted state and sets
    the landing gear if the switch is moved

---
+++ src/modules/events/inverted_state.cpp
@@ -0,0 +1,107 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2018 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file landing_gear.cpp
+ * Landing Gear decouples the landing gear logic form the position controller
+ *
+ * @author Simone Guscetti <simone@px4.io>
+ *
+ */
+
+#include "inverted_state.h"
+#include <px4_log.h>
+
+namespace events
+{
+namespace states
+{
+
+InvertedState::InvertedState(const events::SubscriberHandler &subscriber_handler)
+	: _subscriber_handler(subscriber_handler)
+{
+
+}
+
+bool InvertedState::check_for_updates()
+{
+	bool got_updates = false;
+
+	if (_subscriber_handler.manual_control_sp_updated()) {
+		orb_copy(ORB_ID(manual_control_setpoint), _subscriber_handler.get_manual_control_sp_sub(), &_manual_control_sp);
+
+		got_updates = true;
+	}
+
+	if (_subscriber_handler.vehicle_land_detcted_updated()) {
+		orb_copy(ORB_ID(vehicle_land_detected), _subscriber_handler.get_vehicle_land_detected_sub(), &_land_detector);
+		got_updates = true;
+	}
+
+	return got_updates;
+}
+
+void InvertedState::process()
+{
+	if (!check_for_updates()) {
+		return;
+	}
+
+	if (_land_detector.inverted) {
+		if (_gear_pos_prev != _manual_control_sp.gear_switch) {
+			_landing_gear.landing_gear =
+				(_manual_control_sp.gear_switch == manual_control_setpoint_s::SWITCH_POS_ON) ?
+				landing_gear_s::LANDING_GEAR_UP :
+				landing_gear_s::LANDING_GEAR_DOWN;
+
+			publish();
+		}
+	}
+
+	_gear_pos_prev = _manual_control_sp.gear_switch;
+}
+
+void InvertedState::publish()
+{
+	_landing_gear.timestamp = hrt_absolute_time();
+
+	if (_landing_gear_pub != nullptr) {
+		orb_publish(ORB_ID(landing_gear), _landing_gear_pub, &_landing_gear);
+
+	} else {
+		_landing_gear_pub =  orb_advertise(ORB_ID(landing_gear), &_landing_gear);
+	}
+}
+
+} /* namespace gear */
+} /* namespace events */
---
+++ src/modules/events/inverted_state.h
@@ -0,0 +1,87 @@
+/****************************************************************************
+ *
+ *   Copyright (c) 2018 PX4 Development Team. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ * 3. Neither the name PX4 nor the names of its contributors may be
+ *    used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+ * FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ * COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ * OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+ * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ *
+ ****************************************************************************/
+
+/**
+ * @file landing_gear.h
+ * Landing Gear decouples the landing gear logic form the position controller
+ *
+ * @author Simone Guscetti <simone@px4.io>
+ */
+
+#pragma once
+
+#include "subscriber_handler.h"
+
+#include <drivers/drv_hrt.h>
+
+#include <uORB/uORB.h>
+#include <uORB/topics/landing_gear.h>
+#include <uORB/topics/manual_control_setpoint.h>
+#include <uORB/topics/vehicle_control_mode.h>
+#include <uORB/topics/vehicle_land_detected.h>
+
+namespace events
+{
+namespace states
+{
+class InvertedState
+{
+public:
+	InvertedState(const events::SubscriberHandler &subscriber_handler);
+
+	/** regularily called to handle state updates */
+	void process();
+
+protected:
+	/**
+	 * check for topic updates
+	 * @return true if one or more topics got updated
+	 */
+	bool check_for_updates();
+
+	/** publish LED control */
+	void publish();
+
+	struct manual_control_setpoint_s _manual_control_sp {};
+	struct vehicle_land_detected_s _land_detector {};
+
+	struct landing_gear_s _landing_gear {};
+private:
+	uint8_t _gear_pos_prev = manual_control_setpoint_s::SWITCH_POS_NONE;
+	orb_advert_t _landing_gear_pub{nullptr};
+	const events::SubscriberHandler &_subscriber_handler;
+};
+
+
+} /* namespace gear */
+} /* namespace events */

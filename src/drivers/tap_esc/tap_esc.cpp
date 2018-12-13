6af2b7f8aff97301c550ab8cc83c2822fa89bfab
Alessandro Simovic <alessandro@yuneecresearch.com>
2018-12-13 11:52:36 +0100

    tap_esc: loose prop -> lost prop

--- src/drivers/tap_esc/tap_esc.cpp
+++ src/drivers/tap_esc/tap_esc.cpp
@@ -532,8 +532,8 @@ bool TAP_ESC::esc_critical_failure(uint8_t channel_id)
 			critical_error = true;
 			break;
 
-		case ESC_STATUS_ERROR_LOOSE_PROPELLER:
-			mavlink_log_emergency(&_mavlink_log_pub, "motor %d PROP LOOSE", channel_id);
+		case ESC_STATUS_ERROR_LOST_PROPELLER:
+			mavlink_log_emergency(&_mavlink_log_pub, "motor %d PROP LOST", channel_id);
 			critical_error = true;
 			break;
 

73a9e447dc92c0dadafc2eddd1896777208e8f8b
Alessandro Simovic <alessandro@yuneecresearch.com>
2019-02-05 17:29:23 +0100

    tunes: reduced pause in low-battery tuens

--- src/lib/tunes/yuneec_tunes.cpp
+++ src/lib/tunes/yuneec_tunes.cpp
@@ -9,8 +9,8 @@ const char *const Tunes::_default_tunes[] = {
 	"MFT200e8e", // Notify Neutral tone
 	"MFT200e8c8e8c8e8c8", // Notify Negative tone
 	"MNT75L1O2G", //arming warning
-	"MBT150L16O5CL8O3BP16P16P16L16O5CL8O3BL1PPPPP", //battery warning slow
-	"MBT150L16O5CL8O3BP16P16P16L16O5CL8O3BL1PP", //battery warning fast
+	"MBT150L16O5CL8O3BP16P16P16L16O5CL8O3BL1PPP", //battery warning slow
+	"MBT150L16O5CL8O3BP16P16P16L16O5CL8O3BL1P", //battery warning fast
 	"MFT255L4AAAL1F#", //gps warning slow
 	"MFT255L4<<<BAP", // arming failure tune
 	"MFT255L16agagagag", // parachute release
